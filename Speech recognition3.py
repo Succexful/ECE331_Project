2f0593efa6dea16a10254e6156222f087ec99f3f
Succexful <124508389+Succexful@users.noreply.github.com>
2023-02-06 01:03:19 +0100

    Add files via upload

---
+++ Speech recognition3.py
@@ -0,0 +1,33 @@
+import speech_recognition
+import pyttsx3
+
+sr = speech_recognition.Recognizer()
+
+with speech_recognition.Microphone() as source2:
+
+    print("silence please")
+
+    sr.adjust_for_ambient_noise(source2, duration=2)
+
+    print("speak now please.... ")
+
+    audio2 = sr.listen(source2)
+
+    textt = sr. recognize_google(audio2)
+
+    textt = textt.lower()
+
+    print("Did you say: " + textt)
+
+    
+
+
+
+
+
+
+
+
+
+
+
